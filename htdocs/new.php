3a83bb0b8c97e4098b413ff5a9827524cd374be9
allchannelson <allchannelson@gmail.com>
2016-04-07 18:40:55 +0800

    EOD

--- htdocs/new.php
+++ htdocs/new.php
@@ -3,46 +3,52 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
+<hr>
+<u>Activity</u><br>
+Activity Name: <input type="text" length=40></input><br>
+Date: <input type="text" length=40></input><br>
+Teacher: <input type="text" length=40></input><br>
+<hr>
+<u>Students</u><br>
 <?php
 $mysqli = new mysqli("localhost", "select_only", "xBX8swTSrGawmB5r", "activity_prototype");
 
 /* check connection */
 if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
+    e(sprintf("Connect failed: %s\n", $mysqli->connect_error));
     exit();
 }
 
 /* change encoding */
 if (!$mysqli->set_charset("utf8")) {
-    printf("Error loading character set utf8: %s\n", $mysqli->error);
+    e(sprintf("Error loading character set utf8: %s\n", $mysqli->error));
     exit();
 } else {
     // printf("Current character set: %s\n", $mysqli->character_set_name());
 }
 
+/* custom function for HTML output */
 function e($arg_1) {
    echo(htmlentities($arg_1));
 }
 
-$query = "SELECT * FROM student LIMIT 100";
-?>
-
-<?php
+$query = "SELECT * FROM student LIMIT 10";
+// $query = "SELECT * FROM student";
 
 /* fetch associative array */
 if ($result = $mysqli->query($query)) {
     while ($row = $result->fetch_assoc()) {
       echo("<input type='checkbox'>");
-      // printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
-      // e($row['name_chinese']);
       // $row['student_index'], $row['student_number'], $row['name_chinese'], $row['name_english'], $row['gender'], $row['active']
-      e(sprintf("Index: %d  Student ID: %s  %s  %s  %s  Active: %d",
+      //e(sprintf("Index: %d  Student ID: %s  %s  %s  %s  Active: %d",
+      e(sprintf("Index: %d  Student ID: %s  %s %s",
         $row['student_index'], 
         $row['student_number'], 
         $row['name_chinese'], 
         $row['name_english'],
         $row['gender'], 
-        $row['active']));
+        $row['active'])
+       );
       echo("</input><br>");
 
     }
@@ -52,9 +58,6 @@ if ($result = $mysqli->query($query)) {
     $result->free();
 }
 //
-
-
-
 
 /* close connection */
 $mysqli->close();
