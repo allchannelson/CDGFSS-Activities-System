329c1647643794f817b62edc4d26f83241c84911
allchannelson <allchannelson@gmail.com>
2016-04-21 14:26:14 +0800

    PDO,, submit

--- htdocs/new.php
+++ htdocs/new.php
@@ -1,31 +1,44 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <script type="text/javascript">
+  function validateForm() {
+    var a=document.forms["form"]["activity[name_english]"].value;
+    var b=document.forms["form"]["activity[name_chinese]"].value;
+    var c=document.forms["form"]["activity[date]"].value;
+    var d=document.forms["form"]["activity[teacher]"].value;
+    if ((a==null || a=="") ||
+        (b==null || b=="") ||
+        (c==null || c=="") ||
+        (d==null || d=="")) {
+      alert("Please Fill All Required Field");
+      return false;
+    }
+  }
+  </script>
 </head>
 <body>
 <hr>
-<form action="submit.php" method="post">
+<form name="form" onsubmit="return validateForm()" action="submit.php" method="post">
 <u>Activity</u><br>
-Activity ENG Name: <input type="text" name="activity_name_english" length=100></input><br>
-Date: <input type="text" length=40></input><br>
-Teacher: <input type="text" length=40></input><br>
+Activity ENG Name: <input type="text" name="activity[name_english]" length=100></input><br>
+Activity CHI Name: <input type="text" name="activity[name_chinese]" length=100></input><br>
+Date: <input type="text" name="activity[date]" length=40></input><br>
+Teacher: <input type="text" name="activity[teacher]" length=40></input><br>
 <hr>
 <u>Students</u><br>
 <?php
-$mysqli = new mysqli("localhost", "select_only", "xBX8swTSrGawmB5r", "activity_prototype");
+$dbname = 'activity_prototype';
+$user = 'select_only';
+$password = 'xBX8swTSrGawmB5r';
+$dsn = "mysql:dbname=$dbname;host=localhost;charset=utf8";
 
 /* check connection */
-if ($mysqli->connect_errno) {
-    e(sprintf("Connect failed: %s\n", $mysqli->connect_error));
-    exit();
-}
-
-/* change encoding */
-if (!$mysqli->set_charset("utf8")) {
-    e(sprintf("Error loading character set utf8: %s\n", $mysqli->error));
-    exit();
-} else {
-    // printf("Current character set: %s\n", $mysqli->character_set_name());
+try {
+  $pdo = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+  e(sprintf("Connection failed: %s\n", $e->getMessage()));
+  exit();
 }
 
 /* custom function for HTML output */
@@ -33,36 +46,44 @@ function e($arg_1) {
    echo(htmlentities($arg_1));
 }
 
-$query = "SELECT * FROM student LIMIT 10";
-// $query = "SELECT * FROM student";
-
-/* fetch associative array */
-if ($result = $mysqli->query($query)) {
-  
-  while ($row = $result->fetch_assoc()) {
-    echo(sprintf("<input type='checkbox' name='checkboxArray[]' value='%s | %s'>", $row['student_number'], $row['name_chinese']));
-    // $row['student_index'], $row['student_number'], $row['name_chinese'], $row['name_english'], $row['gender'], $row['active']
-    //e(sprintf("Index: %d  Student ID: %s  %s  %s  %s  Active: %d",
-    e(sprintf("Index: %d  Student ID: %s  %s %s",
-      $row['student_index'], 
-      $row['student_number'], 
-      $row['name_chinese'], 
-      $row['name_english'],
-      $row['gender'], 
-      $row['active'])
-     );
-    echo("</input><br>");
-
-  }
-  echo("</checkbox>");
-
-  /* free result set */
-  $result->free();
+function f($arg_1) {
+   return htmlentities($arg_1);
 }
-//
 
-/* close connection */
-$mysqli->close();
+$query = "SELECT * FROM `student` s
+INNER JOIN `student_yearly_info` syi
+ON s.student_index = syi.student_index
+WHERE syi.enrollment_year = (select max(enrollment_year) from `student_yearly_info`)
+LIMIT 10
+;ORDER BY `form` asc, `class` asc, `class_number` asc
+";
+
+$queryResult = $pdo->query($query);
+
+foreach ($queryResult as $row) {
+  echo(sprintf("<input type='checkbox' name='checkboxArray[]' value='%s,%s'>", $row['student_index'], $row['enrollment_year']));
+  // $row['student_index'], $row['student_number'], $row['name_chinese'], $row['name_english'], $row['gender'], $row['active']
+  // e(sprintf("Index: %d  Student ID: %s  %s  %s  %s  Active: %d",
+  $output = sprintf("Index: %s  Student ID: %s %s %s %s %d %d%s%d %s",
+    f($row['student_index']),
+    f($row['student_number']),
+    f($row['name_chinese']),
+    f($row['name_english']),
+    (strtoupper($row['gender']) == "M"?"<span style='color: blue'>♂</span>":(strtoupper($row['gender']) == "F"?"<span style='color: pink'>♀</span>":"??")),
+    f($row['enrollment_year']),
+    f($row['form']),
+    f($row['class']),
+    f($row['class_number']),
+    f($row['house']),
+    f($row['active']),
+    null);
+  echo($output);
+  echo("</input><br>");
+
+}
+echo("</checkbox>");
+
+$pdo = null;
 ?>
 <input type="submit" value="Submit">
 </form>
--- htdocs/submit.php
+++ htdocs/submit.php
@@ -2,51 +2,65 @@
 <html>
 <body>
 <?php
-$mysqli = new mysqli("localhost", "submit", "DWpZTf5hPf6JjPzq", "activity_prototype");
+$dbname = 'activity_prototype';
+$user = 'submit';
+$password = 'DqcJ3WeWWYQBTG6r';
+$dsn = "mysql:dbname=$dbname;host=localhost;charset=utf8";
 
 /* check connection */
-if ($mysqli->connect_errno) {
-    e(sprintf("Connect failed: %s\n", $mysqli->connect_error));
-    exit();
+try {
+  $pdo = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+  e(sprintf("Connection failed: %s\n", $e->getMessage()));
+  exit();
 }
 
-function myQuery($mysqli, $query, $msg = null) {
-  if (mysqli_query($mysqli, $query)) {
+function myQuery($pdo, $query, $msg = null) {
+  try {
+    $count = $pdo->exec($query);
+    echo "$count rows affected.<br>";
     if (isset($msg)) {
       echo "$msg<br>";
     }
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  } catch (PDFException $e){
+    echo "Error: " . $e->getMessage();
   }
 }
 
-/* change encoding */
-if (!$mysqli->set_charset("utf8")) {
-    e(sprintf("Error loading character set utf8: %s\n", $mysqli->error));
-    exit();
-} else {
-    // printf("Current character set: %s\n", $mysqli->character_set_name());
-}
-
-// $query = "SELECT * FROM student LIMIT 10";
-
-// Input error checking required here, for all required boxes to be filled in.  Probably can implement JavaScript simple layer.
-
 // ***
-if (isset($_POST['activity_name_english'])) {
-  $insertValue = $_POST['activity_name_english'];
-  // INSERT INTO `activity` (`activity_index`, `activity_name_chinese`, `activity_name_english`) VALUES (NULL, NULL, 'Test');
-  $query = "INSERT INTO `activity_prototype`.`activity` (`activity_name_english`) VALUES ('$insertValue')";
-  myQuery($mysqli, $query, "Activity record created successfully!");
-  $query = "SELECT last_insert_id();";
-  myQuery($mysqli, $query, "Last ID query completed.");
-  
+if (isset($_POST['activity']['name_english']) &&
+    isset($_POST['activity']['name_chinese']) &&
+    isset($_POST['activity']['teacher']) &&
+    isset($_POST['activity']['date'])) {
+  foreach ($_POST['activity'] as &$postItem) {
+    $postItem = trim($postItem);
+  }
+  unset($postItem);
+  $ane = $_POST['activity']['name_english'];
+  $anc = $_POST['activity']['name_chinese'];
+  $at  = $_POST['activity']['teacher'];
+  $ad  = $_POST['activity']['date'];
+  if ($_POST['activity']['name_english'] != "") {
+    // Input error checking required here, for all required boxes to be filled in.  Probably can implement JavaScript simple layer.
+    // INSERT INTO `activity` (`activity_index`, `activity_name_chinese`, `activity_name_english`) VALUES (NULL, NULL, 'Test');
+    $query = "INSERT INTO `activity_prototype`.`activity` (`name_english`, `name_chinese`, `date`, `teacher`) VALUES ('$ane', '$anc', '$ad', '$at')";
+    myQuery($pdo, $query, "Activity record created successfully!");
+    
+    $query = "SELECT last_insert_id();";
+    $lastInsertID = $pdo->query($query)->fetch(PDO::FETCH_NUM)[0];
+  }
+} else {
+  echo ("isset 0<br>");
 }
 
 if (isset($_POST['checkboxArray'])) {
-  //echo("Submitted!<br>");
+  // echo("Submitted!<br>");
+  $query = "INSERT into `activity_prototype`.`activity_student` (`activity_index`, `student_index`, `student_enrollment_year`) VALUES ($lastInsertID, :student_index, :student_enrollment_year)";
+  $stmt = $pdo->prepare($query);
   foreach($_POST['checkboxArray'] as $thisCheckbox) {
-    echo($thisCheckbox . "<br>");
+    $studentFields = explode(",",$thisCheckbox);
+    $stmt->execute(array(':student_index' => $studentFields[0], ':student_enrollment_year' => $studentFields[1]));
+    // echo($thisCheckbox . "<br>");
   }
 } else {
   echo("No Data!");
