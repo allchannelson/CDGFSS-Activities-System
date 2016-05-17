cd235e742937c8dcdfa55daf8f8b5bf71d50c458
allchannelson <allchannelson@gmail.com>
2016-05-17 17:29:47 +0800

    Front-end validation and UX update
    Completed front-end JS based validation.  Improved jQuery calls and
    changed to native code.  Added UX options to check/uncheck by class,
    form, gender.

--- htdocs/new.php
+++ htdocs/new.php
@@ -1,12 +1,54 @@
 <html>
 <head>
+  <script src="https://code.jquery.com/jquery-1.12.3.min.js"
+    integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ="   
+  crossorigin="anonymous"></script>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <script type="text/javascript">
+  var StudentCheckboxErrorMessage = "Please Select At Least One Student";
+  validateForm.onChangeBinding = false;
+  function resetForm() {
+    validateForm.onChangeBinding = false;
+    
+    var a=document.forms["form"]["activity[teacher]"];
+    var b=document.forms["form"]["activity[unit]"];
+    var c=document.forms["form"]["activity[name_english]"];
+    var d=document.forms["form"]["activity[name_chinese]"];
+    var e=document.forms["form"]["activity[date]"];
+
+    submitAlertStudentsHTML = document.getElementById("submitAlertStudents");
+    submitAlertHTML = document.getElementById("submitAlert");
+    
+    submitAlertStudentsHTML.innerHTML = "";
+    submitAlertHTML.innerHTML = "";
+    
+    resethighlight(a);
+    resethighlight(b);
+    resethighlight(c);
+    resethighlight(d);
+    resethighlight(e);
+    
+    unbind(a);
+    unbind(b);
+    unbind(c);
+    unbind(d);
+    unbind(e);
+    
+    studentArray = document.getElementsByName("checkboxArray[]");
+    for (i = 0; i < studentArray.length; i++) {
+      unbind(studentArray[i]);
+    }
+
+    document.getElementById("submitButton").disabled = false;
+  }
+  
   function validateForm() {
     var returnValue = true;
     submitAlertStudentsHTML = document.getElementById("submitAlertStudents");
-    submitAlertStudentsHTML.innerHTML = "";
     submitAlertHTML = document.getElementById("submitAlert");
+    
+    // resetForm() is not called because I do not want to unbind the events from the checkboxes
+    submitAlertStudentsHTML.innerHTML = "";
     submitAlertHTML.innerHTML = "";
 
     var a=document.forms["form"]["activity[teacher]"];
@@ -14,7 +56,28 @@
     var c=document.forms["form"]["activity[name_english]"];
     var d=document.forms["form"]["activity[name_chinese]"];
     var e=document.forms["form"]["activity[date]"];
+
+    resethighlight(a);
+    resethighlight(b);
+    resethighlight(c);
+    resethighlight(d);
+    resethighlight(e);
     
+    if (typeof validateForm.onChangeBinding == 'undefined' || validateForm.onChangeBinding == false) {
+      validateForm.onChangeBinding = true;
+      // jQuery version:
+      // $(a).on('input propertychange change click', function() {validateForm();});
+      myBind(a);
+      myBind(b);
+      myBind(c);
+      myBind(d);
+      myBind(e);
+      studentArray = document.getElementsByName("checkboxArray[]");
+      for (i = 0; i < studentArray.length; i++) {
+        myBind(studentArray[i]);
+      }
+    }
+
     if (a.value=="" ||
         b.value=="" ||
         c.value=="" ||
@@ -42,15 +105,45 @@
     
     if (!checkIfStudentsAreSelected()) {
       returnValue = false;
-      submitAlertStudentsHTML.innerHTML = "Please Select At Least One Student";
+      submitAlertStudentsHTML.innerHTML = StudentCheckboxErrorMessage;
+    }
+    
+    submitButtonObj = document.getElementById("submitButton");
+    if (returnValue == false) {
+      submitButtonObj.disabled = true;
+    } else {
+      submitButtonObj.disabled = false;
     }
     
     return returnValue;
   }
   
+  function unbind(element) {
+    element.oninput="";
+    element.onpropertychange="";
+    element.onchange="";
+    element.onclick="";
+  }
+  
+  function myBind(element) {
+    // jQuery equivalent is basically like this:
+    // $(b).on('input propertychange', function() {validateForm();});
+    // but I want to code it without the library for learning
+    element.setAttribute("oninput","validateForm()");
+    element.setAttribute("onpropertychange","validateForm()");
+    element.setAttribute("onchange","validateForm()");
+    element.setAttribute("onclick","validateForm()");
+  }
+  
+  function resethighlight(element) {
+    prevSib = element.previousElementSibling;
+    prevSib.classList.remove("missingLabel");
+  }
+  
   function highlightRed(element) {
     prevSib = element.previousElementSibling;
-    prevSib.style.color="red";
+    // prevSib.style.color="red";
+    prevSib.classList.add("missingLabel");
     submitAlertHTML.innerHTML = submitAlertHTML.innerHTML + " <span class='missingItem'>" + prevSib.innerHTML + "</span>, "
   }
   
@@ -100,6 +193,7 @@
     #submitAlertStudents {
       color: red;
       font-weight: bold;
+      margin-left: 1em;
     }
     
     #submitAlert {
@@ -107,6 +201,11 @@
       color: red;
       font-weight: bold;
     }
+    
+   .missingLabel {
+     color: red;
+     font-weight: bold;
+   }
     
    .missingItem {
       color: blue;
@@ -120,6 +219,51 @@
     .missingItem::before {
       content: "[";
       color: red;
+    }
+    
+    #main {
+      width: 75em;
+    }
+    
+    #left {
+      float: left;
+      width: 40%;
+    }
+    
+    #right {
+      float: right;
+      width: 55%;
+    }
+    
+    .option {
+      float: left;
+      margin: 0.5em;
+    }
+    
+    input[value="Check"], input[value="Uncheck"], input[value="Uncheck All Students"], input[type="Reset"] {
+      display: block;
+    }
+    
+    input[value='Uncheck'] {
+      margin-top: 2em;
+    }
+    input[value='Uncheck All Students'] {
+      margin-top: 3em;
+    }
+    input[type='Reset'] {
+      margin-top: 5em;
+    }
+    
+    .gender {
+      font-size: 1.2em;
+    }
+    
+    .male {
+      color: blue;
+    }
+    
+    .female {
+      color: pink;
     }
   </style>
 </head>
@@ -149,10 +293,11 @@
 
 <span class="mandatory"></span><span style="color: red;"> Mandatory</span>
 <hr>
-<input type="submit" value="Submit"><span id="submitAlert"></span><br><br>
+<input type="submit" id="submitButton" value="Submit"><span id="submitAlert"></span><br><br>
 </div>
-<div id="submitAlertStudents"></div>
-<u>Students</u><br>
+<div id="main">
+<div id="left">
+<u>Students</u><span id="submitAlertStudents"></span><br>
 <?php
 $dbname = 'activity_prototype';
 $user = 'select_only';
@@ -187,7 +332,7 @@ ORDER BY `form` asc, `class` asc, `class_number` asc
 $queryResult = $pdo->query($query);
 
 foreach ($queryResult as $key => $row) {
-  echo(sprintf("<input type='checkbox' name='checkboxArray[]' id = '%s' value='%s,%s' />", $key, $row['student_index'], $row['enrollment_year']));
+  echo(sprintf("<input type='checkbox' name='checkboxArray[]' id='%s' value='%s,%s' class='studentCheck' data-student_form_class='%s%s' data-student_gender='%s' />", $key, $row['student_index'], $row['enrollment_year'], $row['form'], $row['class'], $row['gender']));
   // $row['student_index'], $row['student_number'], $row['name_chinese'], $row['name_english'], $row['gender'], $row['active']
   // e(sprintf("Index: %d  Student ID: %s  %s  %s  %s  Active: %d",
   $output = sprintf("<label for='%s'>S%s%s%s %s %s %s %s %s %s</label>",
@@ -197,7 +342,7 @@ foreach ($queryResult as $key => $row) {
     f($row['class_number']),
     f($row['name_chinese']),
     f($row['name_english']),
-    (strtoupper($row['gender']) == "M"?"<span style='color: blue'>♂</span>":(strtoupper($row['gender']) == "F"?"<span style='color: pink'>♀</span>":"??")),
+    (strtoupper($row['gender']) == "M"?"<span class='gender male'>♂</span>":(strtoupper($row['gender']) == "F"?"<span class='gender female'>♀</span>":"??")),
     f($row['student_number']),
     f($row['enrollment_year']),
     f($row['house']),
@@ -211,6 +356,117 @@ echo("</checkbox>");
 
 $pdo = null;
 ?>
+</div>
+<div id="right">
+<u>Select Multiple Students</u><br>
+<div class="option">
+  Form:
+  <select id="multicheck_form">
+    <option value="">All</option>
+    <option value="1">S1</option>
+    <option value="2">S2</option>
+    <option value="3">S3</option>
+    <option value="4">S4</option>
+    <option value="5">S5</option>
+    <option value="6">S6</option>
+  </select>
+</div>
+<div class="option">
+  Class:
+  <select id="multicheck_class">
+    <option value="">All</option>
+    <option value="A">A</option>
+    <option value="B">B</option>
+    <option value="C">C</option>
+    <option value="D">D</option>
+    <option value="E">E</option>
+  </select>
+</div>
+<div class="option">
+  Gender:
+  <select id="multicheck_gender">
+    <option value="">All</option>
+    <option value="F">F</option>
+    <option value="M">M</option>
+  </select>
+</div>
+<div style="clear: both;">
+  <input type="button" value="Check" onclick="multiCheck()" />
+  <input type="button" value="Uncheck" onclick="multiCheck(true)" />
+  <input type="button" value="Uncheck All Students" onclick="uncheckAllStudents()" />
+  <input type="reset" value="** RESET FORM **" onclick="resetForm()"/>
+</div>
+<script type="text/javascript">
+  function checkAll(elementArr) {
+    for (element in elementArr) {
+      elementArr[element].checked = true;
+    }
+  }
+  
+  function uncheckAll(elementArr) {
+    for (element in elementArr) {
+      elementArr[element].checked = false;
+    }
+  }
+  
+  function uncheckAllStudents() {
+    uncheckAll(document.querySelectorAll('[data-student_form_class]'));
+    if (validateForm.onChangeBinding) {
+      validateForm();
+    }
+  }
+  
+  function multiCheck(uncheck = false) {
+    FormClassSelector = "";  // initialize for consistent behavior... 
+    iForm = document.getElementById("multicheck_form").value;
+    sClass = document.getElementById("multicheck_class").value;
+    sGender = document.getElementById("multicheck_gender").value;
+    if (iForm) {
+      FormClassSelector = iForm;
+    }
+    if (sClass) {
+      if (FormClassSelector !== "") {
+        FormClassSelector += sClass;
+      } else {
+        FormClassSelector = sClass;
+      }
+    }
+    // document.querySelectorAll("[data-student_form_class*='6']")
+    selectorString = "";
+    if (FormClassSelector !== "") {
+      selectorString += "[data-student_form_class*='" + FormClassSelector + "']";
+    }
+    if (sGender) {
+      selectorString += "[data-student_gender='" + sGender + "']";
+    }
+    if (selectorString !== "") {
+      if (uncheck) {
+        uncheckAll(document.querySelectorAll(selectorString));
+      } else {
+        checkAll(document.querySelectorAll(selectorString));
+      }
+    } else {
+      if (uncheck) {
+        uncheckAll(document.querySelectorAll("[data-student_form_class]"));
+      } else {
+        checkAll(document.querySelectorAll("[data-student_form_class]"));
+      }
+    }
+    FormClassSelector = "";
+    
+    if (validateForm.onChangeBinding) {
+      validateForm();
+    }
+  }
+</script>
+
+<!-- closing section for <div id="right"> -->
+</div> 
+
+
+<!-- closing section for <div id="main"> -->
+<span style="clear: both;"></span>
+</div>
 </form>
 </body>
 </html>
