2a07a05a67adfb7b914cbab0fe844c0db6b8495a
allchannelson <allchannelson@gmail.com>
2016-06-29 17:41:52 +0800

    Refactored the mail function as OO

--- htdocs/submit_mail.php
+++ htdocs/submit_mail.php
@@ -1,91 +1,129 @@
 <?php
+// I remade this class as OO, but it can probably just be a helper class and does not need to be instantiated...
+
 require_once 'swift/swift_required.php';
 
 require_once 'models/pdo.php';
 require_once 'models/pdo.cdgfss.php';
 
-$transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
-  ->setUsername('eac_system@school.cdgfss.edu.hk')
-  ->setPassword('7fM5wm$mT[Z=T7+N');
+class cdgfss_mail {
+  
+  private $transport;
 
-$mailer = Swift_Mailer::newInstance($transport);
+  private $mailer;
+  private $activity_id;
+  private $pdoObj;
+  private $textbody;
+  private $htmlbody;
+  private $message;
+  private $activityDetails;
+  private $alternateBGcolor = "style='background: #DDD'";
 
-if (isset($_REQUEST['activity_id'])) {
-  $activity_id = (int)($_REQUEST['activity_id']);
-} else {
-  exit("Invalid/Missing Activity ID: " . (isset($activity_id)?$activity_id:""));
-}
- 
-$pdoObj = new cdgfss_pdo();
-$listActivityStmt = $pdoObj->listActivity_Details($activity_id);
-if ($listActivityStmt->rowCount() <= 0) {
-  exit(); // this should NEVER happen
-}
-$activityDetails =  $listActivityStmt->fetchAll()[0];
+  public function __construct($input) {
+    $this->init();
+    $this->sendMail($input);
+  }
+  
+  private function sendMail($input) {
+    // $input = activity_id
+    $this->setActivityId($input);
+    $this->initDb();
+    $this->generateActivityDetails();
+    $this->generateStudentDetails();
+    $this->generateMessage();
+    $result = $this->mailer->send($this->message);
+  }
 
-$textbody = <<<"TEXTBODY"
--- Activity ID: $activity_id --
-Teacher in charge	{$activityDetails['teacher']}
-Participating Unit	{$activityDetails['unit']}
-Name of Activity / Competition (ENG)	{$activityDetails['name_english']}
-Name of Activity / Competition (CHI)	{$activityDetails['name_chinese']}
-Date	{$activityDetails['date']}
-Time	{$activityDetails['time']}
-Partner Organization (ENG)	{$activityDetails['partner_name_english']}
-Partner Organization (CHI)	{$activityDetails['partner_name_chinese']}
-Destination/Route	{$activityDetails['destination']}
+  private function init() {
+    $this->transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+      ->setUsername('eac_system@school.cdgfss.edu.hk')
+      ->setPassword('7fM5wm$mT[Z=T7+N')
+      ;
+    $this->mailer = Swift_Mailer::newInstance($this->transport);
+  }
+  
+  private function setActivityId($input) {
+    $this->activity_id = (int)$input;
+  }
+  
+  private function initDb() {
+    $this->pdoObj = new cdgfss_pdo();
+  }
+  
+  private function generateStudentDetails() {
+    $activityStudentHeading = $this->pdoObj->columns_Activity_AllStudents();
+    $activityStudents = $this->pdoObj->listActivity_AllStudents($this->activity_id);
+
+    $this->htmlbody .= "<br><hr><p><u>Students</u></p><table border=1 id='studentTable' style='td {border: thin solid red;}'><tr>";
+    $this->textbody .= "** Students **\r\n\r\n";
+    foreach ($activityStudentHeading as $field) {
+      $this->htmlbody .= "<td>$field</td>";
+      $this->textbody .= "$field	";
+    }
+    $count = 0;
+    foreach ($activityStudents as $row) {
+      $this->htmlbody .= $count%2==1?"<tr>":"<tr $this->alternateBGcolor>";
+      $this->textbody .= "\r\n";
+      $count += 1;
+      foreach ($row as $field) {
+        $this->htmlbody .= "<td>$field</td>";
+        $this->textbody .= "$field	";
+      }
+      $this->htmlbody .= "</tr>";
+      $this->textbody .= "\r\n";
+    }
+
+    $this->htmlbody .= "</tr>";
+    $this->htmlbody .= "</table>";
+  }
+
+  private function generateMessage() {
+    $this->message = Swift_Message::newInstance('Activity - ' . $this->activityDetails['name_english'] . '/' . $this->activityDetails['name_chinese'])
+      ->setFrom(array('eac_system@school.cdgfss.edu.hk' => 'EAC System'))
+      ->setTo(array('t15ys@school.cdgfss.edu.hk'))
+      ->setBody($this->textbody)
+      ->addPart($this->htmlbody, 'text/html')
+      ;
+  }
+  
+  private function generateActivityDetails() {
+    $listActivityStmt = $this->pdoObj->listActivity_Details($this->activity_id);
+    if ($listActivityStmt->rowCount() <= 0) {
+      exit(); // this can happen if the activity_id is invalid
+    }
+    $this->activityDetails = $listActivityStmt->fetchAll()[0];
+
+    $this->textbody = <<<"TEXTBODY"
+    -- Activity ID: $this->activity_id --
+    Teacher in charge	{$this->activityDetails['teacher']}
+    Participating Unit	{$this->activityDetails['unit']}
+    Name of Activity / Competition (ENG)	{$this->activityDetails['name_english']}
+    Name of Activity / Competition (CHI)	{$this->activityDetails['name_chinese']}
+    Date	{$this->activityDetails['date']}
+    Time	{$this->activityDetails['time']}
+    Partner Organization (ENG)	{$this->activityDetails['partner_name_english']}
+    Partner Organization (CHI)	{$this->activityDetails['partner_name_chinese']}
+    Destination/Route	{$this->activityDetails['destination']}
 
 
 
 TEXTBODY;
 
-$htmlbody = <<<"HTMLBODY"
+    $this->htmlbody = <<<"HTMLBODY"
 
-<p><u>Activity ID: $activity_id</u></p>
-<table border=1 id='mainTable'>
-  <tr><td>Teacher in charge</td><td>{$activityDetails['teacher']}</td></tr>
-  <tr><td>Participating Unit</td><td>{$activityDetails['unit']}</td></tr>
-  <tr><td>Name of Activity / Competition (ENG)</td><td>{$activityDetails['name_english']}</td></tr>
-  <tr><td>Name of Activity / Competition (CHI)</td><td>{$activityDetails['name_chinese']}</td></tr>
-  <tr><td>Date</td><td>{$activityDetails['date']}</td></tr>
-  <tr><td>Time</td><td>{$activityDetails['time']}</td></tr>
-  <tr><td>Partner Organization (ENG)</td><td>{$activityDetails['partner_name_english']}</td></tr>
-  <tr><td>Partner Organization (CHI)</td><td>{$activityDetails['partner_name_chinese']}</td></tr>
-  <tr><td>Destination/Route</td><td>{$activityDetails['destination']}</td></tr>
-</table>
+    <p><u>Activity ID: $this->activity_id</u></p>
+    <table border=1 id='mainTable'>
+      <tr><td>Teacher in charge</td><td>{$this->activityDetails['teacher']}</td></tr>
+      <tr $this->alternateBGcolor><td>Participating Unit</td><td>{$this->activityDetails['unit']}</td></tr>
+      <tr><td>Name of Activity / Competition (ENG)</td><td>{$this->activityDetails['name_english']}</td></tr>
+      <tr $this->alternateBGcolor><td>Name of Activity / Competition (CHI)</td><td>{$this->activityDetails['name_chinese']}</td></tr>
+      <tr><td>Date</td><td>{$this->activityDetails['date']}</td></tr>
+      <tr $this->alternateBGcolor><td>Time</td><td>{$this->activityDetails['time']}</td></tr>
+      <tr><td>Partner Organization (ENG)</td><td>{$this->activityDetails['partner_name_english']}</td></tr>
+      <tr $this->alternateBGcolor><td>Partner Organization (CHI)</td><td>{$this->activityDetails['partner_name_chinese']}</td></tr>
+      <tr><td>Destination/Route</td><td>{$this->activityDetails['destination']}</td></tr>
+    </table>
 HTMLBODY;
-
-$pdoObj = new cdgfss_pdo();
-$activityStudentHeading = $pdoObj->columns_Activity_AllStudents();
-$activityStudents = $pdoObj->listActivity_AllStudents($activity_id);
-
-$htmlbody .= "<br><hr><p><u>Students</u></p><table border=1 id='studentTable' style='td {border: thin solid red;}'><tr>";
-$textbody .= "** Students **\r\n\r\n";
-foreach ($activityStudentHeading as $field) {
-  $htmlbody .= "<td>$field</td>";
-  $textbody .= "$field	";
-}
-foreach ($activityStudents as $row) {
-  $htmlbody .= "<tr>";
-  $textbody .= "\r\n";
-  foreach ($row as $field) {
-    $htmlbody .= "<td>$field</td>";
-    $textbody .= "$field	";
   }
-  $htmlbody .= "</tr>";
-  $textbody .= "\r\n";
 }
-
-$htmlbody .= "</tr>";
-$htmlbody .= "</table>";
-
-$message = Swift_Message::newInstance('Activity - ' . $activityDetails['name_english'] . '/' . $activityDetails['name_chinese'])
-  ->setFrom(array('eac_system@school.cdgfss.edu.hk' => 'EAC System'))
-  ->setTo(array('t15ys@school.cdgfss.edu.hk'))
-  ->setBody($textbody)
-  ->addPart($htmlbody, 'text/html')
-  ;
-
-$result = $mailer->send($message);
-
 ?>
---
+++ htdocs/submit_mail_test.php
@@ -0,0 +1,7 @@
+<?php
+  require_once 'submit_mail.php';
+  
+  // $asdf = new cdgfss_mail();
+  // $asdf->sendMail(1);
+  $asdf = new cdgfss_mail(1);
+?>
